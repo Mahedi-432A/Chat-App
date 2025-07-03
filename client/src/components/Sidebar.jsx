d42b521bd321e7ccb2790a1b8d9ecb3e0a2432d1
Mahedi-432A <mdmahedih038@gmail.com>
2025-07-03 23:18:42 +0600

    Enhance ChatContainer and Sidebar components with user interaction and message formatting features

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { userDummyData } from '../assets/assets'
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
 
   const navigate = useNavigate()
 
   return (
-    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-auto text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
       <div className='pb-5'>
         <div className='flex items-center justify-between'>
           <img src={assets.logo} alt="logo" className='max-w-40' />
@@ -27,6 +28,25 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
         </div>
 
       </div>
+
+      <div className='flex flex-col'>
+        {userDummyData.map((user, index) => (
+          <div onClick={() => {setSelectedUser(user)}} key={index} className={`flex items-center gap-2 p-2 pl-4 rounded relative cursor-pointer max-sm:text-sm hover:bg-[#282142] ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
+            <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full' />
+            <div className='flex flex-col leading-5'>
+              <p>
+                {user?.fullName}
+              </p>
+              { index < 3
+              ? <span className='text-green-400 text-xs'>Onlione</span>
+              : <span className='text-gray-400 text-xs'>Offline</span>
+              }
+            </div>
+            {index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>}
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
