8000f48d81334533374ee95b18e6006169ce360d
Mahedi-432A <mdmahedih038@gmail.com>
2025-07-03 14:54:36 +0600

    Implement routing and layout structure with Sidebar, ChatContainer, and RightSidebar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import LogIn from './pages/LogIn'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import assets from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+const Sidebar = ({selectedUser, setSelectedUser}) => {
+
+  const navigate = useNavigate()
+
+  return (
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? 'max-md:hidden' : ''}`}>
+      <div className='pb-5'>
+        <div className='flex items-center justify-between'>
+          <img src={assets.logo} alt="logo" className='max-w-40' />
+          <div className='relative py-2 group'>
+            <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
+            <div className='absolute right-0 top-full z-20 w-32 p-5 bg-[#282142] rounded-md text-gray-100 border border-gray-500 hidden group-hover:block'>
+              <p onClick={()=> navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+              <hr className='my-2 border-t border-gray-500' />
+              <p className='cursor-pointer text-sm'>Log Out</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='bg-[#282142] rounded-full flex items-center gap-2 px-4 py-3 mt-5'>
+          <img src={assets.search_icon} alt="search" className='w-3' />
+          <input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' placeholder='Search User...' />
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
